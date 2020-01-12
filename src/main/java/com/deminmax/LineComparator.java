5784fdeefbe1d8a6ada67c51502c3167d2935582
MaksimDemin <mdeminalex@icloud.com>
2020-01-12 23:50:35 +0800

    change Compatators

--- src/main/java/com/deminmax/LineComparator.java
+++ src/main/java/com/deminmax/LineComparator.java
@@ -1,5 +1,4 @@
 package com.deminmax;
-import javax.swing.text.Document;
 import java.util.Comparator;
 
 public class LineComparator implements Comparator<String> {
@@ -7,11 +6,18 @@ public class LineComparator implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        Double s1 = Double.parseDouble(o1.replaceAll("[^0-9]", "\\.41"));
-        Double s2 = Double.parseDouble(o2.replaceAll("[^0-9]", "\\.41"));
+        Double s1, s2;
+        if (o1.matches("\\d+\\D")) {
+            char ch = o1.charAt(o1.length() - 1);
+            s1 = Double.parseDouble(o1.replaceAll("[^0-9]", "")) + Double.parseDouble(Integer.toHexString(ch)) / 1000;
+        }
+        else {s1 = Double.parseDouble(o1);}
 
-        int result = Double.compare(s1, s2);
-        if (result != 0) return result;
+        if (o2.matches("\\d+\\D")) {
+            char ch = o2.charAt(o2.length() - 1);
+            s2 = Double.parseDouble(o2.replaceAll("[^0-9]", "")) + Double.parseDouble(Integer.toHexString(ch)) / 1000;
+        }
+        else {s2 = Double.parseDouble(o2);}
 
         return Double.compare(s1, s2);
     }
--- src/main/java/com/deminmax/LineComparatorInConnections.java
+++ src/main/java/com/deminmax/LineComparatorInConnections.java
@@ -6,11 +6,18 @@ public class LineComparatorInConnections implements Comparator<Station> {
 
     @Override
     public int compare(Station o1, Station o2) {
-        Double s1 = Double.parseDouble(o1.getLine().replaceAll("[^0-9]", "\\.41"));
-        Double s2 = Double.parseDouble(o2.getLine().replaceAll("[^0-9]", "\\.41"));
+        Double s1, s2;
+        if (o1.getLine().matches("\\d+\\D")) {
+            char ch = o1.getLine().charAt(o1.getLine().length() - 1);
+            s1 = Double.parseDouble(o1.getLine().replaceAll("[^0-9]", "")) + Double.parseDouble(Integer.toHexString(ch)) / 1000;
+        }
+        else {s1 = Double.parseDouble(o1.getLine());}
 
-        int result = Double.compare(s1, s2);
-        if (result != 0) return result;
+        if (o2.getLine().matches("\\d+\\D")) {
+            char ch = o2.getLine().charAt(o2.getLine().length() - 1);
+            s2 = Double.parseDouble(o2.getLine().replaceAll("[^0-9]", "")) + Double.parseDouble(Integer.toHexString(ch)) / 1000;
+        }
+        else {s2 = Double.parseDouble(o2.getLine());}
 
         return Double.compare(s1, s2);
     }
